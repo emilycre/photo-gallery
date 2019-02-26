bfe3d86cbf1f61b0a88325303dbc2fe3cb093f64
Emily Creamer <emily.creamer@icloud.com>
2019-02-25 17:46:34 -0800

    Added images to image list using images function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import makeImageFunction from './image-function.js';
+import makeImageTemplate from './image-function.js';
+import images from '../data/images.js';
 
+const imageList = document.getElementById('image-list');
+
+images.forEach(function(image) {
+    const dom = makeImageTemplate(image);
+    imageList.appendChild(dom);
+});
